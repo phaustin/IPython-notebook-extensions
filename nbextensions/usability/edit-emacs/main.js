94ea7e1e2e84839c4514c52b20258f1164885bd2
Phil Austin <paustin@eos.ubc.ca>
2015-09-20 12:09:09 -0700

    working for two keys

--- nbextensions/usability/edit-emacs/main.js
+++ nbextensions/usability/edit-emacs/main.js
@@ -7,29 +7,37 @@ define([
     "use strict";
 
     var add_command_shortcuts = {
-            'ctrl-1' : {
-                help    : 'edit markdown',
-                help_index : 'aa',
-                handler : function() {
-                    IPython.notebook.edit_mode();
+        'ctrl-x': {
+            help    : 'edit markdown',
+            help_index : 'zz',
+            handler : function (event) {
+                var input = IPython.notebook.get_selected_cell().get_text();
+                var cmd = "f = open('temp.md', 'w');f.close()";
+                if (input != "") {
+                    cmd = '%%writefile temp.md\n' + input;
+                }
+                IPython.notebook.kernel.execute(cmd);
+                cmd = "import os;os.system('/Applications/Emacs.app/Contents/MacOS/bin/emacsclient temp.md')";
+                IPython.notebook.kernel.execute(cmd);
+                return false;
+            }
+        },
+        'ctrl-p': {
+                help    : 'edit python',
+                help_index : 'zz',
+                handler : function (event) {
+                    var input = IPython.notebook.get_selected_cell().get_text();
+                    var cmd = "f = open('temp.py', 'w');f.close()";
+                    if (input != "") {
+                        cmd = '%%writefile temp.py\n' + input;
+                    }
+                    IPython.notebook.kernel.execute(cmd);
+                    cmd = "import os;os.system('/Applications/Emacs.app/Contents/MacOS/bin/emacsclient temp.py')";
+                    IPython.notebook.kernel.execute(cmd);
                     return false;
                 }
-            },
-
-            'ctrl-p': {
-                help    : 'edit python',
-                help_index : 'aa',
-                handler : function() {
-                    var index = IPython.notebook.get_selected_index();
-                    if (index !== 0 && index !== null) {
-                        IPython.notebook.extend_selection('up');
-                        IPython.notebook.focus_cell();
-                    }
-                return false;
-                }
             }
-
-        };
+    };
 
     var load_ipython_extension = function() {
         IPython.keyboard_manager.command_shortcuts.add_shortcuts(add_command_shortcuts);
